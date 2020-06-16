344eeb8b45b01f408f9efd1dbca24293556d1137
Cherry <cherry_magdalena@yahoo.co.id>
2020-06-16 17:59:10 +0800

    Added start and stop tracking

--- app/src/main/java/com/example/aadproject/MapsActivity.java
+++ app/src/main/java/com/example/aadproject/MapsActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,6 +16,7 @@ import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -36,6 +38,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -58,21 +62,45 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private final long MIN_DIST = 5;
     private static final int LOCATION_PERMISSION_REQUEST = 1;
     private static final int UPDATE_INTERVAL = 10000; //10 seconds
-    private static final int FASTEST_INTERVAL = 2000; //2 seconds
+    private static final int FASTEST_INTERVAL = 5000; //5 seconds
     private LatLng lastLocation;
-
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private boolean isTracking = false;
+    Button trackingButton;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getSupportActionBar().hide();
+
 //        mMap.setMinZoomPreference(30);
         setContentView(R.layout.activity_maps);
+        trackingButton = findViewById(R.id.button);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Log.d("location callback", "onlocationresult");
+
+                lastLocation = setLocationLatLng(locationResult.getLastLocation());
+                moveCamera(lastLocation);
+
+                if (locationResult == null) {
+                    return;
+                }
+            }
+        };
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
 
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
         int result = apiAvailability.isGooglePlayServicesAvailable(this);
@@ -82,6 +110,29 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 Toast.makeText(this,"Google Play Services not Available", Toast.LENGTH_LONG).show();
             }
         }
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("onLocationChanged","Location changed!");
+                lastLocation = setLocationLatLng(location);
+                moveCamera(lastLocation);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
     }
 
     @Override
@@ -115,9 +166,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void retrieveLastLocation(){
         Log.d("retrieveLastLocation","start");
-        FusedLocationProviderClient fusedLocationProviderClient;
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED){
+         if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED){
             fusedLocationProviderClient.getLastLocation().addOnSuccessListener(
                     new OnSuccessListener<Location>() {
                         @Override
@@ -125,6 +174,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                             Log.d("onSuccess","retrieveLocation");
                             lastLocation = setLocationLatLng(location);
                             moveCamera(lastLocation);
+
 
                         }
                     }
@@ -147,29 +197,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.d("onLocationChanged","Location changed!");
-                lastLocation = setLocationLatLng(location);
-                moveCamera(lastLocation);
-            }
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
 
     }
     private void moveCamera(LatLng latLng){
@@ -180,10 +208,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.addMarker(options);
     }
 
-    private void trackLocation(){
+    public void trackLocation(){
+        Log.d("trackLocation()","startTracking");
         //change comments
         // Create the location request to start receiving updates
-        LocationRequest locationRequest = new LocationRequest();
+        final LocationRequest locationRequest = new LocationRequest();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         locationRequest.setInterval(UPDATE_INTERVAL);
         locationRequest.setFastestInterval(FASTEST_INTERVAL);
@@ -199,38 +228,53 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         settingsClient.checkLocationSettings(locationSettingsRequest);
 
         // new Google API SDK v11 uses getFusedLocationProviderClient(this)
-        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, new LocationCallback() {
-                    @Override
-                    public void onLocationResult(LocationResult locationResult) {
-                        // do work here
-                        Log.d("trackLocation","onLocationResult");
-                        retrieveLastLocation();
-                        Log.d("trackLocation","newLocation ="+lastLocation);
-
-                    }
-                },
-                Looper.myLooper());
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback,Looper.myLooper());
     }
 
     public void stopLocationTracking() {
+        final Task<Void> voidTask = fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        voidTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Log.d("stopLocationTracking()", "addOnCompleteListener: "+task.isComplete());
+            }
+        });
+
+        voidTask.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("stopLocationTracking()", "addOnSuccessListener: success" );
+            }
+        });
+
+        voidTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("stopLocationTracking", "addOnFailureListener: failed");
+            }
+        });
+    }
+
+
+    public void startButtonOnClick(View view) {
+        final Button testButton = findViewById(R.id.button);
+        testButton.setText("Start");
+        testButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                if(isTracking == true) {
+                   stopLocationTracking();
+                    trackingButton.setText("Start");
+                    isTracking=false;
+                } else {
+                   trackLocation();
+                   trackingButton.setText("Stop");
+                   isTracking=true;
+                }
+            }
+        });
 
 
     }
 
-    public void startButtonOnClick(View view)
-    {trackLocation();
-
-    }
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        mGoogleApiClient.connect();
-//    }
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        if (mGoogleApiClient.isConnected()) {
-//            mGoogleApiClient.disconnect();
-//        }
-//    }
 }
